06fc0307464af412603eabd562bca89ab8cb2920
karenbursa <100785136+karenbursa@users.noreply.github.com>
2024-11-16 16:12:20 -0300

    Delete app.module.ts

--- app.module.ts
+++
@@ -1,8 +0,0 @@
-// src/app.module.ts
-import { Module } from '@nestjs/common';
-import { Punto3Module } from './modulo';  // Asegúrate de importar el módulo de punto3 correctamente
-
-@Module({
-  imports: [Punto3Module],  // Aquí debes importar Punto3Module
-})
-export class AppModule {}
